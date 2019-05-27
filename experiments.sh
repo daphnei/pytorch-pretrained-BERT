7c5e61efcb2e2db9416e55bbd6f61dbfbd7081e4
daphne <daphnei@seas.upenn.edu>
2019-05-27 15:18:46 -0400

    run scripts

--- experiments.sh
+++ experiments.sh
@@ -2,15 +2,21 @@
 BEAM_SIZE=10
 MAX_SEQ_LENGTH=50
 
+OUTPUT_DIR="experiments_$BEAM_SIZE"
+
+mkdir $OUTPUT_DIR
+
 # Beam Search
 python examples/run_gpt2.py \
 --inputs_file samples/webtext-test-prompts.txt \
 --nsamples="$BEAM_SIZE" \
 --length="$MAX_SEQ_LENGTH" \
---do_beam_search=True
+--do_beam_search=True \
+--output_file="{$OUTPUT_DIR}/standard_beam.json"
 
 # Random sampling
 python examples/run_gpt2.py \
 --inputs_file samples/webtext-test-prompts.txt \
 --nsamples="$BEAM_SIZE" \
 --length="$MAX_SEQ_LENGTH" \
+-output_file="{$OUTPUT_DIR}/random_sampling.json"
